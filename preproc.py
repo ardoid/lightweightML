d2ddcae5bbf118b68a37997c339a60fb435e469a
ardo <ardo>
2014-01-17 23:01:36 +0100

    Split Data K Folds Cross Validation

--- preproc.py
+++ preproc.py
@@ -6,6 +6,7 @@
 import csv
 import json
 import os.path
+import random
 
 statsFile = 'stats.txt'
 inputFile = 'dataset.csv'
@@ -37,7 +38,7 @@ def splitDataByClass(inputFile):
 			# 	break
 		output = open(statsFile,'w')
 		output.write('Class\t\t\t Size\n')
-		output.write('----------------\n')
+		output.write('------\t\t\t-----\n')
 		idx = 0
 		sums = 0
 		for cl in listOfClass:
@@ -46,14 +47,54 @@ def splitDataByClass(inputFile):
 			idx = idx+1
 		output.write('Total: '+str(sums))
 
-def splitData10fcv(inputFile):
-	print 'a'
+def splitDataKfoldCV(K):
+	stats = open(statsFile,'rb')
+	stats.readline()
+	stats.readline()
+	data = []
+	for line in stats:
+		data.append(line.strip().split('\t\t\t'))
+	stats.close()
+	data.pop() #remove the Total
+	
+	classIdx = 1
+	for d in data:
+		d[1]=int(d[1])
+		print d
+		foldSizeRem = d[1]%K
+		foldSize = d[1]/K + 1
+		print str(foldSize)
+		f = open('o'+str(classIdx)+'.txt')
+		outputFile = []
+		numOfClass = [0 for x in range(K)]
+		for k in range(0,K):
+			fName = 'f'+str(classIdx)+'_'+str(k)+'.txt'
+			output = open(fName,'w')
+			outputFile.append(output)
+		for line in f:
+			nextIdx = random.randrange(K)
+			count = 0
+			while numOfClass[nextIdx] > foldSize and count < 10:
+				nextIdx = nextIdx + 1
+				if nextIdx >= K:
+					nextIdx = 0
+				count = count + 1
+			if count > 9:
+				print 'Oh No'
+			else:
+				outputFile[nextIdx].write(line)
+				# outputFile[indexOfClass].write('\n')
+				numOfClass[nextIdx]=numOfClass[nextIdx]+1
+		classIdx=classIdx+1
+
+
+
 
 #start of the preprocessing program
 if not (os.path.isfile(statsFile)):
 	splitDataByClass(inputFile)
 else:
-	print 'process'
+	splitDataKfoldCV(10)
 
 
 
