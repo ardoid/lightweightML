a2455c339644780ac4b5b4868c18b310372ba6a4
ardo <ardo>
2014-02-05 08:48:02 +0100

    1 vs All and AdaBoost working version

--- ML.py
+++ ML.py
@@ -5,8 +5,13 @@
 #--------------------------------------------------------------#
 from numpy import *
 import operator
+import csv
 
 class ML:
+
+	valMethod=' '
+	iterations=0
+	resultFile='result.csv'
 
 	def __init__(self,im,tm,il,tl):
 		self.inputMatrix=im
@@ -18,15 +23,15 @@ class ML:
 		dataSetSize = self.inputMatrix.shape[0]
 		diffMat = tile(inM, (dataSetSize,1)) - self.inputMatrix
 		sqDiffMat = diffMat**2
-		sqDistances = sqDiffMat.sum(axis=1)
-		distances = sqDistances**0.5
+		sqDist = sqDiffMat.sum(axis=1)
+		distances = sqDist**0.5
 		sortedDistIndicies = distances.argsort()     
-		classCount={}          
+		classCnt={}          
 		for i in range(K):
-		    voteIlabel = self.inputLabel[sortedDistIndicies[i]]
-		    classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-		sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-		return sortedClassCount[0][0]
+		    voteLabel = self.inputLabel[sortedDistIndicies[i]]
+		    classCnt[voteLabel] = classCnt.get(voteLabel,0) + 1
+		sClassCount = sorted(classCnt.iteritems(), key=operator.itemgetter(1), reverse=True)
+		return sClassCount[0][0]
 
 	def kNN(self,K):
 		retList=[]
@@ -39,9 +44,111 @@ class ML:
 			i=i+1
 		# 	if i>50:
 		# 		break
-		# print retList
+		errPct=error/float(self.testMatrix.shape[0])*100
 		print "Test Data: "
 		print self.testMatrix.shape
-		print "Error: "+str(error)+" "+str(error/float(self.testMatrix.shape[0]))+"  K="+str(K)
+		print "Error: "+str(error)+" "+str(errPct)+"  K="+str(K)
+		# toFile=[self.valMethod,str(self.iterations),str(self.inputMatrix.shape[0]),\
+		# 		str(self.testMatrix.shape[0]),str(self.inputMatrix.shape[1]),\
+		# 		str(error),str(errPct),str(K)]
+		toFile=[self.valMethod,self.iterations,self.inputMatrix.shape[0],\
+				self.testMatrix.shape[0],self.inputMatrix.shape[1],error,errPct,K]
+		toFile=str(toFile)
+		with open(self.resultFile,'a') as csvfile:
+			logwriter=csv.writer(csvfile,delimiter=';')
+			logwriter.writerow(toFile)
+
+	def decisionStump(self,inputMatrix,dimen,threshVal,threshIneq):
+	    retArray = ones((shape(inputMatrix)[0],1))
+	    if threshIneq == 'lt':
+	        retArray[inputMatrix[:,dimen] <= threshVal] = -1.0
+	    else:
+	        retArray[inputMatrix[:,dimen] > threshVal] = -1.0
+	    return retArray
+	    
+	def buildStump(self,D):
+	    labelMat=mat(self.inputLabel).T
+	    m,n=shape(self.inputMatrix)
+	    numSteps=10.0; bestStump={}; bestClasEst=mat(zeros((m,1)))
+	    minError=inf
+	    for i in range(n):
+	        rangeMin=self.inputMatrix[:,i].min(); rangeMax = self.inputMatrix[:,i].max();
+	        stepSize=(rangeMax-rangeMin)/numSteps
+	        for j in range(-1,int(numSteps)+1):#loop over all range in current dimension
+	            for inequal in ['lt', 'gt']:
+	                threshVal=(rangeMin + float(j) * stepSize)
+	                predictedVals=self.decisionStump(self.inputMatrix,i,threshVal,inequal)
+	                errArr=mat(ones((m,1)))
+	                errArr[predictedVals==labelMat] = 0
+	                weightedError=D.T*errArr  #calc total error multiplied by D
+#print "split: dim %d, thresh %.2f, thresh ineqal: %s, 
+#the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
+	                if weightedError < minError:
+	                    minError=weightedError
+	                    bestClasEst=predictedVals.copy()
+	                    bestStump['dim']=i
+	                    bestStump['thresh']=threshVal
+	                    bestStump['ineq']=inequal
+	    return bestStump,minError,bestClasEst
+
+	def adaBoostTrain(self,numIt=40):
+	    weakClassArr=[]
+	    m=shape(self.inputMatrix)[0]
+	    D=mat(ones((m,1))/m)
+	    aggClassEst=mat(zeros((m,1)))
+	    classLabels=mat(self.inputLabel)
+	    for i in range(numIt):
+	        bestStump,error,classEst=self.buildStump(D)
+	        #print "D:",D.T
+	        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+	        bestStump['alpha']=alpha  
+	        weakClassArr.append(bestStump) 
+	        #print "classEst: ",classEst.T
+	        expon = multiply(-1*alpha*classLabels.T,classEst)
+	        D=multiply(D,exp(expon)) 
+	        D=D/D.sum()
+	        aggClassEst+=alpha*classEst
+	        #print "aggClassEst: ",aggClassEst.T
+	        aggErrors=multiply(sign(aggClassEst)!=classLabels.T,ones((m,1)))
+	        errorRate=aggErrors.sum()/m
+	        if errorRate==0.0: break
+	    print "total error: ",errorRate
+	    return weakClassArr,aggClassEst
+
+	def adaClassify(self,testMatrix,classifierArr):
+	    m=shape(testMatrix)[0]
+	    aggClassEst=mat(zeros((m,1)))
+	    print classifierArr
+	    for i in range(len(classifierArr)):
+	        classEst=self.decisionStump(testMatrix,\
+	        						 classifierArr[i]['dim'],\
+	                                 classifierArr[i]['thresh'],\
+	                                 classifierArr[i]['ineq'])
+	        print classEst.shape
+	        aggClassEst+=classifierArr[i]['alpha']*classEst
+	    return sign(aggClassEst)
+
+	def adaClassifyTest(self,testMatrix,classifierArr):
+	    m=shape(testMatrix)[0]
+	    aggClassEst=mat(zeros((m,1)))
+	    print classifierArr
+	    for i in range(len(classifierArr)):
+	        classEst=self.decisionStump(testMatrix,\
+	        						 classifierArr[i]['dim'],\
+	                                 classifierArr[i]['thresh'],\
+	                                 classifierArr[i]['ineq'])
+	        print classEst.shape
+	        aggClassEst+=classifierArr[i]['alpha']*classEst
+
+		testLabelM=mat(self.testLabel)
+		m=testLabelM.shape[0]
+	    aggErrors=multiply(sign(aggClassEst)!=testLabelM.T,ones((m,1)))
+	    errorRate=aggErrors.sum()/m
+	    print str(errorRate)
+
+
+	def adaBoost(self):
+		weakClassArr,aggClassEst=self.adaBoostTrain()
+		self.adaClassifyTest(self.testMatrix,weakClassArr)
 
 
--- proc.py
+++ proc.py
@@ -4,16 +4,11 @@
 # Author : Ardo												   #
 #--------------------------------------------------------------#
 
-# import json
-import os.path
-import sys
+# import os.path
 import json
-import random
 import Stats
 import ML
 from numpy import *
-
-resultFile    = 'result.csv'
 
 def loadStats():
 	stats = Stats.Stats()
@@ -30,7 +25,7 @@ def testData(method,param,mlMethod):
 	else:
 		print 'Error: Unknown method'
 
-def loadFromFile(i,j):
+def loadFromFile(i,j,index,il,inMat,sumOfTestData,cls,currClass):
 	fName = 'fn'+str(j)+'_'+str(i)+'.txt'
 	fn = open(fName)
 	lineNum=len(fn.readlines())
@@ -39,15 +34,19 @@ def loadFromFile(i,j):
 	for line in range(0,lineNum):
 		line=fn.readline()
 		line=json.loads(line)
-		learnMat[index,:]=line[0:-1]
-		tl.append(line[-1])
+		inMat[index,:]=line[0:-1]
+		if cls==1:
+			il.append(line[-1])
+		else:
+			if line[-1]==currClass:
+				il.append(1)
+			else:
+				il.append(cls)
 		index+=1
 	fn.close()
-	if sumOfTestData!=testDataSize:
-		learnMat=learnMat[0:sumOfTestData,:]
+	return sumOfTestData,index
 
-
-def loadKFCV(K,mlMethod):
+def loadKFCV(K,mlMethod,cls=1,currClass=''):
 	testDataSize=stats.totalData/K+K
 	for i in range(3,4):
 		numClass=len(stats.classLabel)
@@ -57,18 +56,19 @@ def loadKFCV(K,mlMethod):
 		index=0
 		sumOfTestData=0
 		for j in range(1,numClass+1):
-			fName = 'fn'+str(j)+'_'+str(i)+'.txt'
-			fn = open(fName)
-			lineNum=len(fn.readlines())
-			sumOfTestData+=lineNum
-			fn = open(fName)
-			for line in range(0,lineNum):
-				line=fn.readline()
-				line=json.loads(line)
-				testMat[index,:]=line[0:-1]
-				tl.append(line[-1])
-				index+=1
-			fn.close()
+			# fName = 'fn'+str(j)+'_'+str(i)+'.txt'
+			# fn = open(fName)
+			# lineNum=len(fn.readlines())
+			# sumOfTestData+=lineNum
+			# fn = open(fName)
+			# for line in range(0,lineNum):
+			# 	line=fn.readline()
+			# 	line=json.loads(line)
+			# 	testMat[index,:]=line[0:-1]
+			# 	tl.append(line[-1])
+			# 	index+=1
+			# fn.close()
+			sumOfTestData,index=loadFromFile(i,j,index,tl,testMat,sumOfTestData,cls,currClass)
 		if sumOfTestData!=testDataSize:
 			testMat=testMat[0:sumOfTestData,:]
 
@@ -78,36 +78,50 @@ def loadKFCV(K,mlMethod):
 		ll=[]
 		index=0
 		sumOfTestData=0
-		for l in range(i+1,K):
+		# for l in range(i+1,K):
+		for l in range(K):
+			if l==i:
+				continue
 			for j in range(1,numClass+1):
-				fName = 'fn'+str(j)+'_'+str(l)+'.txt'
-				fn = open(fName)
-				lineNum=len(fn.readlines())
-				sumOfTestData+=lineNum
-				fn = open(fName)
-				for m in range(0,lineNum):
-					line=fn.readline()
-					line=json.loads(line)
-					learnMat[index,:]=line[0:-1]
-					ll.append(line[-1])
-					index+=1
-				fn.close()
+				# fName = 'fn'+str(j)+'_'+str(l)+'.txt'
+				# fn = open(fName)
+				# lineNum=len(fn.readlines())
+				# sumOfTestData+=lineNum
+				# fn = open(fName)
+				# for m in range(0,lineNum):
+				# 	line=fn.readline()
+				# 	line=json.loads(line)
+				# 	learnMat[index,:]=line[0:-1]
+				# 	ll.append(line[-1])
+				# 	index+=1
+				# fn.close()
+				sumOfTestData,index=loadFromFile(l,j,index,ll,learnMat,sumOfTestData,cls,currClass)
 		if sumOfTestData!=testDataSize:
 			learnMat=learnMat[0:sumOfTestData,:]
 
 		print testMat.shape
 		print learnMat.shape
 		ml=ML.ML(learnMat,testMat,ll,tl)
+		ml.valMethod='K-Fold CV'
+		ml.iterations=i
 		if mlMethod=='kNN':
-			ml.kNN(7)
+			ml.kNN(3)
+		elif mlMethod=='adaBoost':
+			ml.adaBoost()
 		else:
 			print 'Default method: 3NN'
 			ml.kNN(3)
 
-def load1vsAll():
+def load1vsAll(mlMethod):
 	numClass=len(stats.classLabel)
-
-	pass
+	for klas in stats.classLabel:
+		loadKFCV(10,mlMethod,-1,klas)
+	
 
 stats=loadStats()
-loadKFCV(10,'kNN')
+# loadKFCV(10,'kNN')
+load1vsAll('adaBoost')
+
+
+
+
